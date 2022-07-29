352c863dc25d114deeb92a58228b72b461e0aa45
YASH MAKWANA <72153542+Yashmakwanab@users.noreply.github.com>
2022-07-29 09:08:22 +0530

    added

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -10,72 +10,31 @@ import './Navbar.css'
 const Navbar = () => {
   const [{ themeName, toggleTheme }] = useContext(ThemeContext)
   const [showNavList, setShowNavList] = useState(false)
-
   const toggleNavList = () => setShowNavList(!showNavList)
-
   return (
     <nav className='center nav'>
-      <ul
-        style={{ display: showNavList ? 'flex' : null }}
-        className='nav__list'
-      >
+      <ul style={{ display: showNavList ? 'flex' : null }} className='nav__list'>
         {projects.length ? (
-          <li className='nav__list-item'>
-            <a
-              href='#projects'
-              onClick={toggleNavList}
-              className='link link--nav'
-            >
-              Projects
-            </a>
-          </li>
+          <li className='nav__list-item'> <a href='#projects' onClick={toggleNavList} className='link link--nav'>Projects</a> </li>
         ) : null}
 
         {skills.length ? (
-          <li className='nav__list-item'>
-            <a
-              href='#skills'
-              onClick={toggleNavList}
-              className='link link--nav'
-            >
-              Skills
-            </a>
-          </li>
+          <li className='nav__list-item'> <a href='#skills'  onClick={toggleNavList}  className='link link--nav'> Skills  </a> </li>
         ) : null}
 
         {contact.email ? (
-          <li className='nav__list-item'>
-            <a
-              href='#contact'
-              onClick={toggleNavList}
-              className='link link--nav'
-           
-            >
-              Contact
-            </a>
-          </li>
+          <li className='nav__list-item'> <a href='#skills'  onClick={toggleNavList}  className='link link--nav'> Contact  </a> </li>
         ) : null}
       </ul>
 
-      <button
-        type='button'
-        onClick={toggleTheme}
-        className='btn btn--icon nav__theme'
-        aria-label='toggle theme'
-      >
+      <button type='button' onClick={toggleTheme} className='btn btn--icon nav__theme' aria-label='toggle theme' >
         {themeName === 'dark' ? <WbSunnyRoundedIcon /> : <Brightness2Icon />}
       </button>
 
-      <button
-        type='button'
-        onClick={toggleNavList}
-        className='btn btn--icon nav__hamburger'
-        aria-label='toggle navigation'
-      >
+      <button type='button' onClick={toggleNavList} className='btn btn--icon nav__hamburger' aria-label='toggle navigation' >
         {showNavList ? <CloseIcon /> : <MenuIcon />}
       </button>
     </nav>
   )
 }
-
 export default Navbar
